50a6124551332e9382e9be287d0c337847092b3b
Suzana Maranhão Moreno <suzana.moreno@Suzanas-MacBook-Pro.local>
2021-08-21 22:19:43 -0300

    requreAuth

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -1,11 +1,11 @@
 module.exports = (req, resp, next) => {
 
-    const backendPassword = "ofslafsdewcdsfsdplpAjmiDdfdsq!d";
+    const backendPassword = process.env.API_KEY;
 
-    const {authorization} = req.headers;
+    const { authorization } = req.headers;
 
     console.log("autorizacao");
 
-    if (!authorization || authorization!= backendPassword) resp.status(401).send("Wrong password - Not authorized");
+    if (!authorization || authorization != backendPassword) resp.status(401).send("Wrong password - Not authorized");
     next();
 }
